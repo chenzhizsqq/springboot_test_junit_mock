59ec70b3f143c196041f3a636610a106427e26f6
chenzhizsqq <chenzhi@qq.com>
2023-10-03 10:58:03 +0900

    first commit

---
+++ src/test/java/com/example/springboot_test_junit_mock/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springboot_test_junit_mock.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("okokokok");
+	}
+
+}
