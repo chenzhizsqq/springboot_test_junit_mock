bcfa0099e4a411e1cbf96620ae3f0e4a8a72fd17
chenzhizsqq <chenzhi@qq.com>
2023-10-03 12:25:42 +0900

    添加mockito

--- src/test/java/com/example/springboot_test_junit_mock/demo/DemoApplicationTests.java
+++ src/test/java/com/example/springboot_test_junit_mock/demo/DemoApplicationTests.java
@@ -2,6 +2,10 @@ package com.example.springboot_test_junit_mock.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class DemoApplicationTests {
